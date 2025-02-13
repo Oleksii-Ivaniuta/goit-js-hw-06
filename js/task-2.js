ab07cd2e84303826217681d8d9e7d10367bebdbc
Oleksii Ivaniuta <oivaniuta@gmail.com>
2025-02-13 19:04:51 +0000

    fixed task 2

--- js/task-2.js
+++ js/task-2.js
@@ -13,14 +13,9 @@ class Storage {
     }
 
     removeItem(itemToRemove) {
-        for (let item of this.#items) {
-            if (item === itemToRemove) {
-                this.#items = this.#items.filter(item => item !== itemToRemove);
-            }
+ this.#items = this.#items.filter(item => item !== itemToRemove);
         }
-
     }
-}
 
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
